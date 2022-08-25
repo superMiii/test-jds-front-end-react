779461bc28a68e1594a875dd345fe33d464b8226
superMiii <fahminurfalah26@gmail.com>
2022-08-26 06:00:14 +0700

    feat add form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,32 +1,73 @@
 function Form() {
   return (
-    <div className="form-input">
-      <form id="inputData">
-        <div>
-          <label>Nama Lengkap</label>
-          <input type="text" id="namaLengkap" placeholder="Nama Lengkap" />
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabIndex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Modal title
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div className="form-input">
+              <form id="inputData">
+                <div>
+                  <label>Nama Lengkap</label>
+                  <input
+                    type="text"
+                    id="namaLengkap"
+                    placeholder="Nama Lengkap"
+                  />
+                </div>
+                <div>
+                  <label>NIK</label>
+                  <input type="text" id="namaLengkap" placeholder="NIK" />
+                </div>
+                <div>
+                  <label>NO KK</label>
+                  <input type="text" id="namaLengkap" placeholder="NO KK" />
+                </div>
+                <div>
+                  <label>Foto KTP</label>
+                  <input type="file" id="namaLengkap" placeholder="Foto KTP" />
+                </div>
+                <div>
+                  <label>Foto KK</label>
+                  <input type="file" id="namaLengkap" placeholder="Foto KK" />
+                </div>
+                <div>
+                  <label>Umur</label>
+                  <input type="umur" id="umur" placeholder="Umur" />
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </div>
         </div>
-        <div>
-          <label>NIK</label>
-          <input type="text" id="namaLengkap" placeholder="NIK" />
-        </div>
-        <div>
-          <label>NO KK</label>
-          <input type="text" id="namaLengkap" placeholder="NO KK" />
-        </div>
-        <div>
-          <label>Foto KTP</label>
-          <input type="file" id="namaLengkap" placeholder="Foto KTP" />
-        </div>
-        <div>
-          <label>Foto KK</label>
-          <input type="file" id="namaLengkap" placeholder="Foto KK" />
-        </div>
-        <div>
-          <label>Umur</label>
-          <input type="umur" id="umur" placeholder="Umur" />
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
--- src/components/ListData.js
+++ src/components/ListData.js
@@ -1,19 +1,47 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Form from "./Form";
+
 export const ListData = () => {
+  const [listData, setListData] = useState(localStorage.getItem("listData"));
   return (
     <>
       <h1 className="mt-4">Data Bantuan Covid-19</h1>
       <ol className="breadcrumb mb-4">
         <li className="breadcrumb-item active">List data</li>
       </ol>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Add Data
+      </button>
+
       <div className="row">
-        <div className="col">
+        <div className="table-responsive">
           <table className="table">
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Nama</th>
+                <th scope="col">NIK</th>
+                <th scope="col">Nomor KK</th>
+                <th scope="col">Foto KTP</th>
+                <th scope="col">Foto KK</th>
+                <th scope="col">Umur</th>
+                <th scope="col">Jenis Kelamin</th>
+                <th scope="col">Provinsi</th>
+                <th scope="col">Kab/Kota</th>
+                <th scope="col">Kecamatan</th>
+                <th scope="col">Kelurahan</th>
+                <th scope="col">Alamat</th>
+                <th scope="col">RT</th>
+                <th scope="col">RW</th>
+                <th scope="col">Penghasilan Sebelum Pandemi</th>
+                <th scope="col">Penghasilan Setelah Pandemi</th>
+                <th scope="col">Alasan membutuhkan bantuan</th>
               </tr>
             </thead>
             <tbody>
@@ -22,22 +50,26 @@ export const ListData = () => {
                 <td>Mark</td>
                 <td>Otto</td>
                 <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
               </tr>
             </tbody>
           </table>
         </div>
       </div>
+      <Form />
     </>
   );
 };
